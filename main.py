c5bd2306caa00c8fc09501d290dc9805650da6a8
Wyesumu <wyesumu@gmail.com>
2019-10-24 17:01:06 +0700

    added tables in sqlalchemy

--- main.py
+++ main.py
@@ -15,21 +15,82 @@ from flask_bcrypt import Bcrypt
 '''
 Connection
 '''
-proxy_url = "http://proxy.server:3128"
+
 bot = telebot.TeleBot(config.token, threaded=False)
-bot.set_webhook("https://Wyesumu.pythonanywhere.com/{}".format(config.secret))
+bot.set_webhook(config.addr)
 
 app = flask.Flask(__name__)
 
 bcrypt = Bcrypt(app)
-app.secret_key = config.secret_key
+app.secret_key = config.secret
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+config.db_user+':'+config.db_pass+'@'+config.db_host+'/'+config.db_name + '?charset=utf8mb4'
 app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 db = SQLAlchemy(app)
 
+user_association = db.Table('user_association', db.Model.metadata,
+	db.Column('button_id', db.Integer, db.ForeignKey('button.id'), primary_key=True),
+	db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
 
 
+class Admin(db.Model):
+
+	__table_args__ = { 'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8', 'mysql_collate': 'utf8_general_ci' }
+
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(255))
+	password = db.Column(db.String(255))
+	privileges = db.Column(db.Integer, default = 5)
+
+	def __str__(self):
+		return self.username
+
+class Channel(db.Model):
+
+	__table_args__ = { 'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8', 'mysql_collate': 'utf8_general_ci' }
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(255, collation='utf8_unicode_ci'))
+	posts = db.relationship('Post', backref='channel', lazy=True)
+
+	def __str__(self):
+		return self.name
+
+class Post(db.Model):
+
+	__table_args__ = { 'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8', 'mysql_collate': 'utf8_general_ci' }
+
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.String(255, collation='utf8_unicode_ci'))
+	image_addr = db.Column(db.String(255, collation='utf8_unicode_ci'))
+	chanel_id = db.Column(db.Integer, db.ForeignKey("channel.id"), nullable=False)
+	buttons = db.relationship('Button', backref='post', lazy=True)
+
+	def __str__(self):
+		return self.text
+
+class Button(db.Model):
+
+	__table_args__ = { 'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8', 'mysql_collate': 'utf8_general_ci' }
+
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.String(255, collation='utf8_unicode_ci'))
+	details = db.Column(db.String(255, collation='utf8_unicode_ci'))
+	post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+	users = db.relationship("User", secondary=user_association, lazy='subquery',
+							backref=db.backref('users', lazy=True))
+
+	def __str__(self):
+		return self.text
+
+class User(db.Model):
+
+	__table_args__ = { 'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8', 'mysql_collate': 'utf8_general_ci' }
+
+	id = db.Column(db.Integer, primary_key=True)
+
+db.create_all()
 
 @app.route('/{}'.format(config.secret), methods=["POST"])
 def webhook():
