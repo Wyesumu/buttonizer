812fdc2f0f587f1e3511d04ac7e68e0efe4f4f76
Wyesumu <wyesumu@gmail.com>
2019-10-25 12:14:58 +0700

    Added many-to-many relationship for admin-channel

--- main.py
+++ main.py
@@ -35,6 +35,11 @@ user_association = db.Table('user_association', db.Model.metadata,
 	db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
 )
 
+author_association = db.Table('author_association', db.Model.metadata,
+	db.Column('channel_id', db.Integer, db.ForeignKey('channel.id'), primary_key=True),
+	db.Column('author_id', db.Integer, db.ForeignKey('author.id'), primary_key=True)
+)
+
 class Author(db.Model):
 
 	__table_args__ = { 'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8', 'mysql_collate': 'utf8_general_ci' }
@@ -54,6 +59,8 @@ class Channel(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(255, collation='utf8_unicode_ci'))
 	posts = db.relationship('Post', backref='channel', lazy=True)
+	authors = db.relationship("Author", secondary=author_association, lazy='subquery',
+							backref=db.backref('channel', lazy=True))
 
 	def __str__(self):
 		return self.name
@@ -117,6 +124,64 @@ def webhook():
         return ''
     else:
         flask.abort(403)
+
+#site start
+#--------------------<session control>----------------------
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	if flask.request.method == 'POST':
+		#get data from form
+		login = flask.request.form["login"] 
+		password = flask.request.form["password"]
+		user_data = Author.query.filter_by(username = login).first()
+		if user_data is not None:
+			if bcrypt.check_password_hash(user_data.password, str(password)): #compare user input and password hash from db
+				#set session info in crypted session cookie
+				flask.session['logged'] = "yes"
+				flask.session['user_id'] = user_data.id
+				return flask.redirect(flask.url_for("index"))
+			else:
+				flask.flash("Wrong password. Try again")
+		else:
+			flask.flash("Wrong Login. Try again")
+	return flask.render_template("login.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	if flask.request.method == 'POST':
+		f = flask.request.form.to_dict(flat=False) #get data from form
+	
+		if [''] in f.values(): #if there's any empty field
+			flask.flash("Не все поля заполнены")
+			return flask.redirect(flask.url_for("register"))
+		elif f['password'][0] != f['password_check'][0]: #if passwords doesn't match
+			flask.flash("Пароли не совпадают")
+			return flask.redirect(flask.url_for("register"))
+		elif f['pass_phrase'][0] != config.pass_phrase: #if passphrase is wrong
+			flask.flash('Неверно введена кодовая фраза')
+			return flask.redirect(flask.url_for("register"))
+		else:
+			#check if username already in db
+			if Author.query.filter_by(username = f["login"][0]).first():
+				flask.flash("Пользователь с таким именем уже существует")
+				return flask.redirect(flask.url_for("register"))
+			else:
+				#create new user
+				new_user = Author(username = f["login"][0], password = bcrypt.generate_password_hash(str(f["password"][0])))
+				db.session.add(new_user)
+				db.session.commit()
+
+		return flask.redirect(flask.url_for("login"))
+	else:
+		return flask.render_template("register.html")
+
+@app.route("/exit", methods=["GET"])
+def exit():
+	flask.session.clear() #clear session when user log out
+	return flask.redirect(flask.url_for("login"))
+
+#---------------------/session control/--------------------------
 
 #bot start
 @bot.message_handler(content_types=['photo', 'audio', 'sticker'])
